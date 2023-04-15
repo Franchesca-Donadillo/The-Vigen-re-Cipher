f22934ee6e42ba10aa4fda181e85bd816a57ff77
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-16 00:58:12 +0800

    running vigenre cipher

--- The Vigenre cipher.py
+++ The Vigenre cipher.py
@@ -23,5 +23,40 @@ console.print("="*60, "\n\n" + message_input.center(55), style = "input")
 console.print(key_input.center(55), "\n\n" + "="*60, style = "input")
 
 # generating the key 
+def Key(message, keyword):
+    keyword = list(keyword)
+    if len(message) == len(keyword):
+        return(keyword)
+    else:
+        for i in range(len(message) - len(keyword)):
+            keyword.append(keyword[i])
+    return("" . join(keyword))
+
 # encrypt text
+def cipherNum(message, key):
+    cipher_text = []
+    for i in range(len(message)):
+        add_mod = (ord(message[i]) + ord(key[i])) % 26
+        add_mod += 0
+        cipher_text.append(str(add_mod))
+    return(" " . join(cipher_text))
+
+def cipherText(message, key):
+    cipher_text = []
+    for i in range(len(message)):
+        add_mod = (ord(message[i]) + ord(key[i])) % 26
+        add_mod += ord('A')
+        cipher_text.append(chr(add_mod))
+    return(" " . join(cipher_text))
+     
+message = message_input
+keyword = key_input
+key = Key(message, keyword)
+cipher_text = cipherText(message,key)
+num = cipherNum(message, key)
+
 # print ciphertext
+print("Loading Loading Loading.... \n")
+console.print("✔ Untranslated numbers :", num + "\n", style = "num_num")
+console.print(":heart: The ciphertext :", cipher_text + ":heart: \n", style = "cipher")
+
