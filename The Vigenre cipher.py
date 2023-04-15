ac004b1e2c9f0f58bbb75e85b93c4834fddb75f9
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-16 00:46:21 +0800

    user input uppercase

--- The Vigenre cipher.py
+++ The Vigenre cipher.py
@@ -15,7 +15,13 @@ new_title = title.center(55).upper()
 console.print(new_title, style = "theTitle")
 
 # ask user for input message and key
+message_input = input("Enter your message: ").upper()
+key_input = input("Enter your key: ").upper()
+
 # print uppercase letters of the user input
+console.print("="*60, "\n\n" + message_input.center(55), style = "input")
+console.print(key_input.center(55), "\n\n" + "="*60, style = "input")
+
 # generating the key 
 # encrypt text
 # print ciphertext
